0dd694a60d15c48a5cd3f21349b4e3142d78a7c7
Asif Ali <asifali94851@gmail.com>
2026-07-22 16:08:07 +0530

    Render students using StudentList

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import Header from "./components/Header";
-import StudentCard from "./components/StudentCard";
+import StudentList from "./components/StudentList";
+import students from "./data/students.json";
 
 function App() {
-  const student = {
-    id: 1,
-    name: "Aarav Sharma",
-    age: 21,
-    email: "aarav.sharma@example.com",
-    course: "React Native",
-    enrollmentYear: 2024,
-    gpa: 8.4,
-  };
-
   return (
     <>
       <Header />
-      <StudentCard student={student} />
+      <StudentList students={students} />
     </>
   );
 }
